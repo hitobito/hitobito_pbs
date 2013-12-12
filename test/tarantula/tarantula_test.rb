be2af5c90b91bc4d6a72c79d023a6a683a1548bf
Pascal Zumkehr <zumkehr@puzzle.ch>
2013-12-12 11:21:00 +0100

    add license headers

--- test/tarantula/tarantula_test.rb
+++ test/tarantula/tarantula_test.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2013, Puzzle ITC GmbH. This file is part of
-#  hitobito_generic and licensed under the Affero General Public License version 3
+#  Copyright (c) 2012-2013, Pfadibewegung Schweiz. This file is part of
+#  hitobito_pbs and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_generic.
+#  https://github.com/hitobito/hitobito_pbs.
 
 require "test_helper"
 require "relevance/tarantula"
