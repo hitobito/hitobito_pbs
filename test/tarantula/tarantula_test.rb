91ea219e7ce2aa4318d8b0c8402a498603277b31
Pascal Zumkehr <zumkehr@puzzle.ch>
2014-09-11 09:55:16 +0200

    centralize tarantula config in core

--- test/tarantula/tarantula_test.rb
+++ test/tarantula/tarantula_test.rb
@@ -7,6 +7,7 @@
 
 require 'test_helper'
 require 'relevance/tarantula'
+require 'tarantula/tarantula_config'
 
 class TarantulaTest < ActionDispatch::IntegrationTest
   # Load enough test data to ensure that there's a link to every page in your
@@ -16,6 +17,7 @@ class TarantulaTest < ActionDispatch::IntegrationTest
 
   reset_fixture_path File.expand_path('../../../spec/fixtures', __FILE__)
 
+  include TarantulaConfig
 
   def test_tarantula_as_bundesleitung
     crawl_as(people(:bulei))
@@ -29,64 +31,19 @@ class TarantulaTest < ActionDispatch::IntegrationTest
     crawl_as(people(:child))
   end
 
-  def crawl_as(person)
-    person.password = 'foobar'
-    person.save!
-    post '/users/sign_in', person: { email: person.email, password: 'foobar' }
-    follow_redirect!
+  private
 
-    t = tarantula_crawler(self)
-    # t.handlers << Relevance::Tarantula::TidyHandler.new
+  def configure_urls_with_pbs(t, person)
+    configure_urls_without_pbs(t, person)
 
-    # some links use example.com as a domain, allow them
-    t.skip_uri_patterns.delete(/^http/)
-    t.skip_uri_patterns << /^http(?!:\/\/www\.example\.com)/
-    t.skip_uri_patterns << /year=201[0-15-9]/ # only 2012 - 2014
-    t.skip_uri_patterns << /year=200[0-9]/
-    t.skip_uri_patterns << /year=202[0-9]/
-    t.skip_uri_patterns << /users\/sign_out/
-    # no modifications of user roles (and thereof its permissions)
-    t.skip_uri_patterns << /groups\/\d+\/roles\/(#{person.roles.collect(&:id).join("|")})$/
-    # no ajax links in application market
-    t.skip_uri_patterns << /groups\/\d+\/events\/\d+\/application_market\/\d+\/participant$/
+    # The parent entry may already have been deleted, thus producing 404s.
+    t.allow_404_for(/groups\/\d+\/member_counts$/)
 
-    # The type or merge_group_id tarantula generates is not from the
-    # given selection, thus producing 404s.
-    t.allow_404_for /groups$/
-    t.allow_404_for /groups\/\d+\/roles$/
-    t.allow_404_for /groups\/\d+\/roles\/\d+$/
-    t.allow_404_for /groups\/\d+\/people$/
-    t.allow_404_for /groups\/\d+\/merge$/
-    t.allow_404_for /groups\/\d+\/move$/
-    t.allow_404_for /groups\/\d+\/events$/
-    t.allow_404_for /groups\/\d+\/events\/\d+$/
-    t.allow_404_for /groups\/\d+\/events\/\d+\/roles$/
-    t.allow_404_for /groups\/\d+\/events\/\d+\/roles\/\d+$/
-    t.allow_404_for /groups\/\d+\/mailing_lists\/\d+\/subscriptions\/person$/
-    t.allow_404_for /groups\/\d+\/mailing_lists\/\d+\/subscriptions\/event$/
-    t.allow_404_for /groups\/\d+\/mailing_lists\/\d+\/subscriptions\/exclude_person$/
-    t.allow_404_for /groups\/\d+\/mailing_lists\/\d+\/subscriptions\/\d+$/
-    t.allow_404_for /event_kinds\/\d+$/
-    t.allow_404_for /event_kinds$/
-    t.allow_404_for /groups\/\d+\/member_counts$/
-    # kind already deleted in another language
-    t.allow_404_for /qualification_kinds\/\d+$/
-    # groups already deleted in another language
-    t.allow_404_for /it\/groups\/\d+$/
-    # custom return_urls end up like that.
-    t.allow_404_for /^\-?\d+$/
-
-    # sphinx not running
-    t.allow_500_for /full$/
-    # delete qualification is not allowed after role was removed from person
-    t.allow_500_for /groups\/\d+\/people\/\d+\/qualifications\/\d+$/
     # delete not allowed - not completely clarified - investigate later
-    t.allow_500_for /groups\/\d+\/events\/\d+\/roles\/\d+$/
+    t.allow_500_for(/groups\/\d+\/events\/\d+\/roles\/\d+$/)
     # roles with invalid created_at and deleted_at values may generate 500 :(
-    t.allow_500_for /groups\/\d+\/roles(\/\d+)?$/
-
-
-    t.crawl_timeout = 20.minutes
-    t.crawl
+    t.allow_500_for(/groups\/\d+\/roles(\/\d+)?$/)
   end
+  alias_method_chain :configure_urls, :pbs
+
 end
