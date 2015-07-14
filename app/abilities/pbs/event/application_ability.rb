1845cdf3bb0ec0ddd5bbc251b9de425e0e8306f7
Andreas Maierhofer <maierhofer@puzzle.ch>
2015-07-14 14:53:41 +0200

    abilities and specs for :show_approvals, refs #11571

--- app/abilities/pbs/event/application_ability.rb
+++ app/abilities/pbs/event/application_ability.rb
@@ -7,19 +7,48 @@
 
 module Pbs::Event::ApplicationAbility
   extend ActiveSupport::Concern
+  include AbilityDsl::Constraints::Event::Participation
 
   included do
     on(Event::Application) do
-      permission(:approve_applications).may(:approve, :reject).for_approvals_in_layer
+      permission(:approve_applications).may(:approve, :reject).for_approvals_in_same_layer
+      permission(:approve_applications).may(:show_approvals).for_approvals_in_hierarchy
+
+      permission(:any).may(:show_approvals).for_participations_full_events
+      permission(:group_full).may(:show_approvals).in_same_group
+      permission(:layer_full).may(:show_approvals).in_same_layer_or_different_prio
+      permission(:layer_and_below_full).may(:show_approvals).in_same_layer_or_below_or_different_prio
     end
   end
 
-  def for_approvals_in_layer
-    if subject.next_open_approval && subject.participation.person.primary_group
-      approving_roles = subject.next_open_approval.roles
-      layer_ids = subject.participation.person.primary_group.layer_hierarchy.collect(&:id)
+  def for_approvals_in_same_layer
+    if primary_group && next_open_approval
+      approving_roles = next_open_approval.roles
       user.roles.any? { |role| approving_roles.include?(role.class) && layer_ids.include?(role.group_id) }
     end
+  end
+
+  def for_approvals_in_hierarchy
+    if primary_group
+      approving_roles = user.roles.select { |r| r.permissions.include?(:approve_applications) }
+      contains_any?(layer_ids, approving_roles.collect(&:group_id))
+    end
+  end
+
+  def next_open_approval
+    subject.next_open_approval
+  end
+
+  def primary_group
+    participation.person.primary_group
+  end
+
+  def layer_ids
+    primary_group.layer_hierarchy.collect(&:id)
+  end
+
+  def participation
+    subject.participation
   end
 
 end
--- spec/abilities/event/application_ability_spec.rb
+++ spec/abilities/event/application_ability_spec.rb
@@ -80,4 +80,95 @@ describe Event::ApplicationAbility do
 
   end
 
+
+  context 'showing approvals' do
+
+    context 'approve_applications permission' do
+      [ %w(bund Geschaeftsleitung),
+        %w(be Kantonsleitung),
+        %w(bern Regionalleitung),
+        %w(patria Abteilungsleitung)].each do |name, role|
+          it "#{role} in #{name} may show approvals for participant of patria" do
+            group = groups(name)
+            create_approver("#{group.type}::#{role}".constantize, group)
+            create_application(Group::Abteilung::Praeses, groups(:patria), 'bund')
+            is_expected.to be_able_to(:show_approvals, application)
+          end
+        end
+
+      it 'Kantonsleitung in zh may not show approvals for participant of patria' do
+        create_approver(Group::Kantonalverband::Kantonsleitung, groups(:zh))
+        create_application(Group::Abteilung::Praeses, groups(:patria), 'bund')
+        is_expected.not_to be_able_to(:show_approvals, application)
+      end
+    end
+
+    context 'leader and participants' do
+      it 'course leader may show_approvals' do
+        @approver = event_roles(:top_leader)
+        create_application(Group::Bund::Mitarbeiter, groups(:bund), 'bund')
+        is_expected.to be_able_to(:show_approvals, application)
+      end
+
+      it 'course participant may not show_approvals' do
+        create_application(Group::Bund::Mitarbeiter, groups(:bund), 'bund')
+        @approver = application.participation
+        is_expected.not_to be_able_to(:show_approvals, application)
+      end
+
+      it 'other course participant may not show_approvals' do
+        @approver = event_roles(:top_tn)
+        create_application(Group::Bund::Mitarbeiter, groups(:bund), 'bund')
+        is_expected.not_to be_able_to(:show_approvals, application)
+      end
+    end
+
+    context 'group and layers' do
+      it ':group_full of organising group may show_approvals' do
+        course.groups = [groups(:pegasus)]
+        course.save!
+
+        create_approver(Group::Pfadi::Adressverwaltung, groups(:pegasus))
+        create_application(Group::Bund::Mitarbeiter, groups(:bund), 'bund')
+        is_expected.to be_able_to(:show_approvals, application)
+      end
+
+      it ':layer_full of may show approvals of same layer group' do
+        create_approver(Group::Kantonalverband::VerantwortungAusbildung, groups(:be))
+        create_application(Group::Bund::Mitarbeiter, groups(:bund), 'bund')
+        is_expected.to be_able_to(:show_approvals, application)
+      end
+
+      it ':layer_full of may show approvals of same layer' do
+        course.groups << groups(:zh)
+        create_approver(Group::Kantonalverband::VerantwortungAusbildung, groups(:zh))
+        create_application(Group::Bund::Mitarbeiter, groups(:bund), 'bund')
+        is_expected.to be_able_to(:show_approvals, application)
+      end
+
+      it ':layer_full of may not show approvals of layer below' do
+        create_approver(Group::Bund::AssistenzAusbildung, groups(:bund))
+        create_application(Group::Bund::Mitarbeiter, groups(:bund), 'bund')
+        is_expected.not_to be_able_to(:show_approvals, application)
+      end
+
+      it ':layer_full_and_below may show approvals of layer below' do
+        create_approver(Group::Bund::MitarbeiterGs, groups(:bund))
+        create_application(Group::Bund::Mitarbeiter, groups(:bund), 'bund')
+        is_expected.to be_able_to(:show_approvals, application)
+      end
+
+      it ':layer_full_and_below may show approvals of different layer when application is on waiting list' do
+        course.groups = [groups(:zuerich)]
+        course.save!
+
+        create_approver(Group::Region::Regionalleitung, groups(:bern))
+        create_application(Group::Region::Mitarbeiter, groups(:zuerich), 'bund')
+
+        application.update(waiting_list: true)
+        is_expected.to be_able_to(:show_approvals, application)
+      end
+    end
+  end
+
 end
