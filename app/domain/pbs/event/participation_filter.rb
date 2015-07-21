d53baba683ed460ae037ad17f6191d36a91666ad
Pascal Zumkehr <zumkehr@puzzle.ch>
2015-07-21 13:57:16 +0200

    only show revoked in courses for authorized users

--- app/domain/pbs/event/participation_filter.rb
+++ app/domain/pbs/event/participation_filter.rb
@@ -9,18 +9,27 @@ module Pbs::Event::ParticipationFilter
 
   extend ActiveSupport::Concern
 
-  REVOKED_STATES =
-
   included do
-    Event::ParticipationFilter::PREDEFINED_FILTERS << 'revoked'
-
     self.load_entries_includes += [:application]
 
+    alias_method_chain :predefined_filters, :revoked
     alias_method_chain :apply_filter_scope, :revoked
   end
 
+  def predefined_filters_with_revoked
+    @predefined_filters ||=
+      predefined_filters_without_revoked.dup.tap do |predefined|
+        if event.supports_applications? &&
+           Ability.new(user).can?(:index_participations_details, event)
+          predefined << 'revoked'
+        end
+      end
+  end
+
+  private
+
   def apply_filter_scope_with_revoked(records, kind = params[:filter])
-    if kind == 'revoked'
+    if kind == 'revoked' && predefined_filters.include?('revoked')
       event.participations.
             joins(:roles).
             where('event_roles.type' => event.participant_types.collect(&:sti_name)).
--- spec/domain/event/participation_filter_spec.rb
+++ spec/domain/event/participation_filter_spec.rb
@@ -12,6 +12,7 @@ describe Event::ParticipationFilter do
   let(:event) { events(:top_course) }
   let(:user) { people(:bulei) }
   let(:filter) { Event::ParticipationFilter.new(event, user, filter: tab)}
+  let(:tab) { nil }
 
   before do
     @canceled = create_participant(state: 'canceled', canceled_at: Date.today)
@@ -20,6 +21,30 @@ describe Event::ParticipationFilter do
     @applied = create_participant(state: 'applied')
     @leader = event_participations(:top_leader)
     @assigned = event_participations(:top_participant)
+  end
+
+  context 'predefined_filters' do
+    context 'for course' do
+      it 'contains revoked' do
+        expect(filter.predefined_filters).to include('revoked')
+      end
+    end
+
+    context 'for simple events' do
+      let(:event) { events(:top_event) }
+
+      it 'does not contain revoked' do
+        expect(filter.predefined_filters).not_to include('revoked')
+      end
+    end
+
+    context 'for course participant' do
+      let(:user) { people(:al_schekka) }
+
+      it 'does not contain revoked' do
+        expect(filter.predefined_filters).not_to include('revoked')
+      end
+    end
   end
 
   context 'revoked' do
