96a0a21fc5a5f0b0cfbb02ddff279991b40ec3f0
Pascal Zumkehr <zumkehr@puzzle.ch>
2015-08-11 20:11:55 +0200

    reset column information

--- db/migrate/20150805094000_add_people_kv.rb
+++ db/migrate/20150805094000_add_people_kv.rb
@@ -2,6 +2,8 @@ class AddPeopleKv < ActiveRecord::Migration
   def change
     add_column :people, :kantonalverband_id, :integer
 
+    Person.reset_column_information
+
     Person.find_each(&:reset_kantonalverband!)
   end
 end
