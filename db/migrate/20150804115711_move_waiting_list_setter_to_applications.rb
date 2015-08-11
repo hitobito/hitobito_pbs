6706429c6ca8789eeb1afac99541ab5abc2af499
Pascal Zumkehr <zumkehr@puzzle.ch>
2015-08-11 20:21:19 +0200

    reset column information

--- db/migrate/20150804115711_move_waiting_list_setter_to_applications.rb
+++ db/migrate/20150804115711_move_waiting_list_setter_to_applications.rb
@@ -2,6 +2,9 @@ class MoveWaitingListSetterToApplications < ActiveRecord::Migration
   def up
     remove_column(:event_participations, :waiting_list_setter_id)
     add_column(:event_applications, :waiting_list_setter_id, :integer)
+
+    Event::Participation.reset_column_information
+    Event::Application.reset_column_information
   end
 
   def down
