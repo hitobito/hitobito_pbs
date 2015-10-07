712c89a8792e642925d4c93087ce49124a63dc8f
Mathis Hofer <hofer@puzzle.ch>
2015-10-07 15:35:35 +0200

    declare documents_text attribute of event kind as translated, refs #11554, #11553

---
+++ app/models/pbs/event/kind.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+
+#  Copyright (c) 2012-2015, Pfadibewegung Schweiz. This file is part of
+#  hitobito_pbs and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pbs.
+
+module Pbs::Event::Kind
+  extend ActiveSupport::Concern
+
+  included do
+    translates :documents_text
+  end
+end
