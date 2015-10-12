7993abf236c47314517062bee8c884ba93d22eab
Mathis Hofer <hofer@puzzle.ch>
2015-10-12 12:42:44 +0200

    move camp spec to event directory, add j_s_kind tests, refs #13097

--- spec/models/event/camp_spec.rb
+++ spec/models/event/camp_spec.rb
@@ -32,7 +32,28 @@ describe Event::Camp do
       subject.camp_days = 4
       is_expected.to be_valid
     end
+  end
 
+  context '#j_s_kind' do
+    it 'accepts empty value' do
+      subject.j_s_kind = nil
+      is_expected.to be_valid
+
+      subject.j_s_kind = ''
+      is_expected.to be_valid
+    end
+
+    %w(j_s_child j_s_youth j_s_mixed).each do |kind|
+      it "accepts '#{kind}'" do
+        subject.j_s_kind = kind
+        is_expected.to be_valid
+      end
+    end
+
+    it 'does not accept any other string' do
+      subject.j_s_kind = 'asdf'
+      is_expected.not_to be_valid
+    end
   end
 
 end
