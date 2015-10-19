c81e8ce13d55b7c126ce262b8155016909296901
Pascal Zumkehr <zumkehr@puzzle.ch>
2015-10-19 17:51:23 +0200

    add some pdf layouting methods for camp application, refs #13209, #13210

--- app/domain/export/pdf/camp_application.rb
+++ app/domain/export/pdf/camp_application.rb
@@ -12,7 +12,7 @@ module Export::Pdf
 
     attr_reader :camp, :pdf
 
-    delegate :text, :font_size, :move_down,
+    delegate :text, :font_size, :move_down, :text_box, :cursor, :table,
              to: :pdf
 
     def initialize(camp)
@@ -23,7 +23,7 @@ module Export::Pdf
       @pdf = Prawn::Document.new(page_size: 'A4',
                                  page_layout: :portrait,
                                  margin: 2.cm)
-      pdf.font_size 9
+      pdf.font_size 10
 
       render_sections
 
@@ -45,8 +45,9 @@ module Export::Pdf
       render_title
       render_group
       render_leader
-      render_assistand_leaders
-      render_info
+      render_assistant_leaders
+      render_dates
+      render_location
       render_j_s
       render_state
       render_abteilungsleitung
@@ -62,19 +63,37 @@ module Export::Pdf
 
     def render_group
       section('group_header') do
-
       end
     end
 
     def render_leader
+      section('leader_header') do
+        leader = camp.participations_for(Event::Camp::Role::Leader).first.try(:person)
+        if leader
+          with_label('name', leader.to_s)
+          with_label('address', leader.address)
+          with_label('zip_town', [leader.zip_code, leader.town].compact.join(' '))
+          with_label('birthday', I18n.l(leader.birthday))
+          with_label('qualifications', active_qualifications(leader))
+        end
+      end
+    end
+
+    def render_assistant_leaders
+      section('assistant_leaders_header') do
+        leaders = camp.participations_for(Event::Camp::Role::AssistantLeader).collect(&:person)
+        cells = leaders.collect do |person|
+          [person.to_s, person.birthday.year.to_s, active_qualifications(person)]
+        end
+        table(cells, width: 500, cell_style: { border_width: 0.25 })
+      end
+    end
+
+    def render_dates
 
     end
 
-    def render_assistand_leaders
-
-    end
-
-    def render_info
+    def render_location
 
     end
 
@@ -102,11 +121,28 @@ module Export::Pdf
     end
 
     def heading
-      font_size(11) { yield }
+      font_size(14) { yield }
     end
 
-    def move_down_line(line = 10)
+    def with_label(key, value)
+      label = translate(key)
+      text_box(label, at: [0, cursor], width: 120, style: :italic)
+      text_box(value, at: [120, cursor])
+      move_down_line
+    end
+
+    def move_down_line(line = 12)
       move_down(line)
+    end
+
+    def active_qualifications(person)
+      QualificationKind.joins(:qualifications).
+                        where(qualifications: { person_id: person.id }).
+                        merge(Qualification.active).
+                        uniq.
+                        list.
+                        collect(&:to_s).
+                        join("\n")
     end
 
   end
