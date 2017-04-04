035c73c3294eda9f91dc243893aed4233c3fcff0
Mathis Hofer <hofer@puzzle.ch>
2017-04-04 11:28:29 +0200

    add approved approvals to pending approvals page refs #33

--- app/controllers/group/pending_approvals_controller.rb
+++ app/controllers/group/pending_approvals_controller.rb
@@ -11,12 +11,18 @@ class Group::PendingApprovalsController < ApplicationController
 
   before_action :authorize_action
 
+  helper_method :course_kind_id
+
   def index
-    @approvals =
-      Event::Approval.pending(group).
-      includes(:participation, :approvee, event: [:dates, :groups]).
-      order('event_participations.created_at ASC')
+    load_pending_approvals
     @approver_roles = approver_roles
+    load_approved_course_kinds
+    load_approved_approvals
+  end
+
+  def approved
+    load_approved_course_kinds
+    load_approved_approvals
   end
 
   def update_role
@@ -32,8 +38,41 @@ class Group::PendingApprovalsController < ApplicationController
     @group ||= Group.find(params[:id])
   end
 
+  def load_pending_approvals
+    @pending_approvals =
+      Event::Approval.pending(group).
+      includes(:participation, :approvee, event: :groups).
+      order('event_participations.created_at ASC')
+  end
+
   def approver_roles
     group.class.role_types.select { |t| t.permissions.include?(:approve_applications) }
+  end
+
+  def load_approved_approvals
+    @approved_approvals ||=
+      approved_approvals.where(course_kind_id.present? && ['events.kind_id = ?', course_kind_id])
+                        .order(approved_at: :desc)
+                        .page(params[:page])
+                        .per(10)
+  end
+
+  def load_approved_course_kinds
+    @approved_course_kinds ||=
+      Event::Kind.includes(:translations)
+                 .where(id: approved_approvals.distinct.pluck('events.kind_id'))
+  end
+
+  def approved_approvals
+    Event::Approval
+      .includes(:participation, :approvee, event: :groups)
+      .joins(approvee: :primary_group, application: { participation: :event })
+      .where('groups.lft >= :lft AND groups.rgt <= :rgt', lft: group.lft, rgt: group.rgt)
+      .where(layer: group.class.name.demodulize.downcase, approved: true)
+  end
+
+  def course_kind_id
+    params[:course_kind_id]
   end
 
   def authorize_action
