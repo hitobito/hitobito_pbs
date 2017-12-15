75cdf921eb9f3a98b3d85ee116499f4c187f1fed
Matthias Viehweger <viehweger@puzzle.ch>
2017-12-15 21:06:02 +0100

    Remove duplicated LICENSE-statement

--- spec/models/member_count_spec.rb
+++ spec/models/member_count_spec.rb
@@ -30,12 +30,6 @@
 #  pta_m              :integer
 #
 
-
-#  Copyright (c) 2012-2013, Jungwacht Blauring Schweiz. This file is part of
-#  hitobito_jubla and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_jubla.
-
 require 'spec_helper'
 
 describe MemberCount do
