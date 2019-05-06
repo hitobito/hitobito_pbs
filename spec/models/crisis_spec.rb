1630146a484bebfc06eb76c7f917347e75b67165
Andreas Maierhofer <a.maierhofer@gmail.com>
2019-05-06 14:34:44 +0200

    Create crisis model

---
+++ spec/models/crisis_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe Crisis do
+  context 'validation' do
+    it 'cannot create second active crisis in same group' do
+      crisis = Crisis.create(group: groups(:bund),
+                             creator: people(:bulei))
+
+      expect(crisis).to have(1).errors_on(:base)
+      expect(crisis.errors.full_messages.join("/n")).
+        to match(/Eine andere Krise ist bereits aktiv auf dieser Gruppe/)
+    end
+  end
+end
