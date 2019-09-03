9b4a3fdd2c4c786a8caaa16df43e2c570cb369ed
cdn64 <gitcdn@gmail.com>
2019-09-03 09:29:22 +0200

    Adjust migration and spec based on review

--- db/migrate/20190820060509_change_event_camp_submitted_to_date.rb
+++ db/migrate/20190820060509_change_event_camp_submitted_to_date.rb
@@ -1,17 +1,13 @@
 class ChangeEventCampSubmittedToDate < ActiveRecord::Migration
-  def change
-    reversible do |dir|
-      dir.up do
-        add_column :events, :camp_submitted_at, :date
-        execute("UPDATE events SET camp_submitted_at=created_at WHERE camp_submitted=true")
-        remove_column :events, :camp_submitted
-      end
+  def up
+    add_column :events, :camp_submitted_at, :date
+    execute("UPDATE events SET camp_submitted_at=created_at WHERE camp_submitted=true")
+    remove_column :events, :camp_submitted
+  end
 
-      dir.down do
-        add_column :events, :camp_submitted, :boolean, null: false, default: false
-        execute("UPDATE events SET camp_submitted=true WHERE camp_submitted_at NOT NULL")
-        remove_column :events, :camp_submitted_at
-      end
-    end
+  def down
+    add_column :events, :camp_submitted, :boolean, null: false, default: false
+    execute("UPDATE events SET camp_submitted=true WHERE camp_submitted_at NOT NULL")
+    remove_column :events, :camp_submitted_at
   end
 end
