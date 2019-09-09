224ae76f4a65211712bae4f9c00b800bd5573db8
Carlo Beltrame <carlo@beltra.me>
2019-09-09 14:19:50 +0200

    Add test for geolocation model
    Refs hitobito/hitobito#585

---
+++ spec/models/geolocation_spec.rb
@@ -0,0 +1,28 @@
+#  Copyright (c) 2019, Pfadibewegung Schweiz. This file is part of
+#  hitobito_pbs and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pbs.
+
+# == Schema Information
+#
+# Table name: geolocations
+#
+#  id                     :integer          not null, primary key
+#  lat                    :string
+#  long                   :string
+#  geolocatable_id        :integer          not null
+#  geolocatable_type      :string           not null
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+
+require 'spec_helper'
+
+describe Geolocation do
+
+  it 'to_s' do
+    subject = Fabricate(:geolocation)
+    expect(subject.to_s).to eq("(#{subject.lat}, #{subject.long})")
+  end
+
+end
