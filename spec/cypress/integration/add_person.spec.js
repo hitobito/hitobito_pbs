54cc87f93df607d759056fcac4b8eab4b9c2e772
Nicolas Da Mutten <nicolas@damutten.ch>
2019-09-22 10:09:07 +0200

    Improve add_person.spec.js

--- spec/cypress/integration/add_person.spec.js
+++ spec/cypress/integration/add_person.spec.js
@@ -1,21 +1,24 @@
 describe('The user', function () {
-	beforeEach(() => {
-		cy.login("kunz@puzzle.ch", "hito42bito")
-	})
-	it('can add a new person', function () {
-		cy.login('hitobito-pbs@puzzle.ch', 'hito42bito')
-		cy.visit('de/groups/1/roles/new')
-		cy.get('a:contains("Neue Person erfassen")').click()
-		cy.get('#role_new_person_first_name')
-			.type('Marlies')
-		cy.get('#role_new_person_last_name')
-			.type('Sauerkraut')
-		cy.get('#role_new_person_nickname')
-			.type('Brexit')
-		cy.get('#role_new_person_email')
-			.type('marlies_sk88@example.com')
-		cy.get('#role_type_select').click()
-		cy.get('div.chosen-search input').type("Gesch{enter}")
-		cy.get('form#new_role button[type="submit"]:first').click()
-	})
+  beforeEach(() => {
+    cy.login("kunz@puzzle.ch", "hito42bito")
+  })
+  it('can add a new person', function () {
+    cy.visit('de/groups/1/roles/new')
+
+    cy.get('a:contains("Neue Person erfassen")').click()
+
+    cy.get('#role_new_person_first_name')
+      .type('Marlies')
+    cy.get('#role_new_person_last_name')
+      .type('Sauerkraut')
+    cy.get('#role_new_person_nickname')
+      .type('Brexit')
+    cy.get('#role_new_person_email')
+      .type('marlies_sk88@example.com')
+
+    cy.get('#role_type_select').click()
+    cy.get('div.chosen-search input').type("Gesch{enter}")
+
+    cy.get('form#new_role button[type="submit"]:first').click()
+  })
 })
