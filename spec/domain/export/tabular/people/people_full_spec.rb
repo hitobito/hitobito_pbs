58535b6f89547037f4e07505dba6b459649d2b0d
Matthias Viehweger <viehweger@puzzle.ch>
2021-11-08 15:18:45 +0100

    Spec only select, added attributes
    This should keep the spec more stable.

--- spec/domain/export/tabular/people/people_full_spec.rb
+++ spec/domain/export/tabular/people/people_full_spec.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-#  Copyright (c) 2012-2017, Jungwacht Blauring Schweiz. This file is part of
+#  Copyright (c) 2012-2021, Jungwacht Blauring Schweiz. This file is part of
 #  hitobito and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito.
@@ -15,17 +15,34 @@ describe Export::Tabular::People::PeopleFull do
 
   subject { people_list }
 
-  let(:full_attributes) do
-    [:first_name, :last_name, :company_name, :nickname,
-     :company, :email, :address, :zip_code, :town, :country,
-     :gender, :birthday, :additional_information, :nationality_j_s,
-     :ahv_number, :j_s_number, :salutation, :title, :grade_of_school,
-     :entry_date, :leaving_date, :correspondence_language,
-     :brother_and_sisters, :kantonalverband_id,
-     :pbs_number, :prefers_digital_correspondence, :layer_group,
-     :roles, :tags, :id, :layer_group_id]
-  end
+  context 'attributes include' do
+    it 'id' do
+      expect(subject.attributes).to include(:id)
+    end
 
-  its(:attributes) { should match_array full_attributes }
+    it 'layer-group id' do
+      expect(subject.attributes).to include(:layer_group_id)
+    end
+
+    it 'layer-group (name)' do
+      expect(subject.attributes).to include(:layer_group)
+    end
+
+    it 'tags' do
+      expect(subject.attributes).to include(:tags)
+    end
+
+    it 'prefers_digital_correspondence' do
+      expect(subject.attributes).to include(:prefers_digital_correspondence)
+    end
+
+    it 'kantonalverband_id' do
+      expect(subject.attributes).to include(:kantonalverband_id)
+    end
+
+    it 'pbs_number' do
+      expect(subject.attributes).to include(:pbs_number)
+    end
+  end
 
 end
