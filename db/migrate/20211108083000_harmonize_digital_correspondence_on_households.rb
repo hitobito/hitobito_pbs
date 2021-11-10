2d388078bdcd33ab565be8ac09ae8dbdc58b768c
Carlo Beltrame <carlo@beltra.me>
2021-11-10 14:28:03 +0100

    Fix migration so that it really takes all households into account
    Previously, we patched find_in_batches so that it would order by
    household member count descendingly. But that never really worked,
    because find_in_batches doesn't use SQL OFFSET internally, but rather
    "WHERE id > largest_id_of_previous_batch" in order to step through the
    batches. So here, we are forced to re-implement find_in_batches for our
    use case with some modifications.

--- db/migrate/20211108083000_harmonize_digital_correspondence_on_households.rb
+++ db/migrate/20211108083000_harmonize_digital_correspondence_on_households.rb
@@ -1,3 +1,8 @@
+#  Copyright (c) 2021, Pfadibewegung Schweiz. This file is part of
+#  hitobito_pbs and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pbs.
+
 class HarmonizeDigitalCorrespondenceOnHouseholds < ActiveRecord::Migration[6.0]
   FLAG = :prefers_digital_correspondence
 
@@ -29,33 +34,23 @@ class HarmonizeDigitalCorrespondenceOnHouseholds < ActiveRecord::Migration[6.0]
       @people_scope = people_scope
     end
 
-    def people_without_household
-      @people_scope.where(household_key: nil)
-    end
-
     def grouped_households
       people = Person.quoted_table_name
 
-      # remove previously added selects, very important to make this query scale
-      @people_scope.unscope(:select, :includes).select( # group by household, but keep NULLs separate
-        "IFNULL(#{people}.`household_key`, #{people}.`id`) as `key`"
-      )
-        .select("MIN(#{Person.quoted_table_name}.`id`) as `id`") # Must select the primary key column because find_in_batches needs it for sorting.
-        .select("COUNT(#{people}.`household_key`) as `count`")
-        .group(:key)
+      @people_scope.
+        # remove previously added selects, very important to make this query scale
+        unscope(:select, :includes).
+        # group by household, but keep NULLs separate
+        select("IFNULL(#{people}.`household_key`, #{people}.`id`) as `key`").
+        group(:key).
+        # Primary sorting criterion
+        select("COUNT(#{people}.`household_key`) as `member_count`").
+        # Secondary, unique sorting criterion
+        select("MIN(#{people}.`id`) as `id`")
     end
 
     def households_in_batches
-      base_scope = only_households
-
-      # When limiting the number of rows, make sure to always show some households.
-      # For this, we override the order that find_in_batches uses. By default, it
-      # always orders by the primary key column ascendingly.
-      def base_scope.batch_order
-        [Arel::Nodes::SqlLiteral.new("`count`").desc, super]
-      end
-
-      base_scope.find_in_batches(batch_size: 300) do |batch|
+      in_batches(only_households, batch_size: 300) do |batch|
         involved_people =
           fetch_people_with_id_or_household_key(batch.map(&:key))
         grouped_people =
@@ -71,26 +66,66 @@ class HarmonizeDigitalCorrespondenceOnHouseholds < ActiveRecord::Migration[6.0]
       end
     end
 
-    def each(exclude_non_households: false, &block)
-      return to_enum(:each) unless block
-
-      households_in_batches(
-        exclude_non_households: exclude_non_households
-      ) { |batch| batch.each(&block) }
-    end
-
     private
 
     def only_households
       grouped_households.where.not(household_key: nil)
     end
 
+    # Copied and adapted from ActiveRecord::Batches#in_batches
+    # We need to order and "offset" the batches by two separate columns, which
+    # activerecord doesn't support natively. Activerecord only supports ordering by the
+    # primary key column and doesn't use SQL OFFSET internally, for performance reasons:
+    def in_batches(base_scope, batch_size: 1000)
+      relation = base_scope
+
+      batch_limit = batch_size
+      if base_scope.limit_value
+        remaining   = base_scope.limit_value
+        batch_limit = remaining if remaining < batch_limit
+      end
+
+      relation = relation.reorder('`member_count` DESC, id ASC').limit(batch_limit)
+      # Retaining the results in the query cache would undermine the point of batching
+      relation.skip_query_cache!
+      batch_relation = relation
+
+      loop do
+        records = batch_relation.records
+        ids = records.map(&:id)
+        yielded_relation = base_scope.where(id: ids)
+        yielded_relation.send(:load_records, records)
+
+        break if ids.empty?
+
+        member_count_offset = records.last.member_count
+        id_offset = ids.last
+
+        yield yielded_relation
+
+        break if ids.length < batch_limit
+
+        if base_scope.limit_value
+          remaining -= ids.length
+
+          if remaining == 0
+            # Saves a useless iteration when the limit is a multiple of the batch size.
+            break
+          elsif remaining < batch_limit
+            relation = relation.limit(remaining)
+          end
+        end
+
+        batch_relation = relation.having('`member_count` < ? OR (`member_count` = ? AND `id` > ?)',
+                                        member_count_offset, member_count_offset, id_offset)
+      end
+    end
+
     def fetch_people_with_id_or_household_key(keys_or_ids)
       # Search for any number of housemates, regardless of preview limit
-      base_scope = @people_scope.unscope(:limit) # Make sure to select household_key if we aren't selecting specific columns
-      if base_scope.select_values.present?
-        base_scope = base_scope.select(:household_key)
-      end
+      base_scope = @people_scope.unscope(:limit)
+      # Make sure to select household_key if we aren't selecting specific columns
+      base_scope = base_scope.select(:household_key) if base_scope.select_values.present?
 
       base_scope.where(household_key: keys_or_ids).or(
         base_scope.where(id: keys_or_ids)
