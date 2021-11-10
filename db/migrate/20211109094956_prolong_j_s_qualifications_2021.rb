8fad71b13011447f415091bb429dc575d0901211
Mountain Star <github@mtnstar.net>
2021-11-10 14:43:51 +0100

    Add frozen string literal to migration file

--- db/migrate/20211109094956_prolong_j_s_qualifications_2021.rb
+++ db/migrate/20211109094956_prolong_j_s_qualifications_2021.rb
@@ -1,7 +1,9 @@
-#  Copyright (c) 2021, Pfadibewegung Schweiz. This file is part of
-#  hitobito_pbs and licensed under the Affero General Public License version 3
-#  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito_pbs.
+# frozen_string_literal: true
+
+# Copyright (c) 2021, Pfadibewegung Schweiz. This file is part of
+# hitobito_pbs and licensed under the Affero General Public License version 3
+# or later. See the COPYING file at the top-level directory or at
+# https://github.com/hitobito/hitobito_pbs.
 
 class ProlongJSQualifications2021 < ActiveRecord::Migration[6.0]
 
