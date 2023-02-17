618ab7c0c859dcd4e6e5054bacb222e62c2cd3ae
Matthias Viehweger <viehweger@puzzle.ch>
2023-02-17 11:01:04 +0100

    Keep active-state if participation stays active

--- app/domain/pbs/event/participant_assigner.rb
+++ app/domain/pbs/event/participant_assigner.rb
@@ -17,9 +17,17 @@ module Pbs::Event::ParticipantAssigner
   private
 
   def set_active_with_camp(active)
-    state = participation.event.send(:default_participation_state, participation)
+    default_state = participation.event.send(:default_participation_state, participation)
 
-    participation.update!(active: active, state: active ? state : 'applied')
+    active_state_from_youth_wagon = 'assigned'
+    active_state = if participation.state == active_state_from_youth_wagon
+                     active_state_from_youth_wagon
+                   else
+                     default_state
+                   end
+    inactive_state = 'applied'
+
+    participation.update!(active: active, state: active ? active_state : inactive_state)
   end
 
   def remove_from_waiting_list_with_setter
