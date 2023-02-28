d8bd7b16aafe6fc35cd52da994eb82058cb61c03
Carlo Beltrame <carlo@beltra.me>
2023-02-28 16:41:56 +0100

    PBS-specific adjustments to #1966

--- app/abilities/pbs/person_ability.rb
+++ app/abilities/pbs/person_ability.rb
@@ -12,11 +12,11 @@ module Pbs::PersonAbility
     on(Person) do
       permission(:any)
         .may(:show, :show_full, :show_details, :history,
-             :index_tags, :index_notes).if_member_of_crisis_group_or_oneself
+             :index_tags, :index_notes).if_member_of_crisis_group_or_oneself_or_manager
     end
 
-    def if_member_of_crisis_group_or_oneself
-      herself || member_of_crisis_group
+    def if_member_of_crisis_group_or_oneself_or_manager
+      herself || manager || member_of_crisis_group
     end
 
     def member_of_crisis_group
