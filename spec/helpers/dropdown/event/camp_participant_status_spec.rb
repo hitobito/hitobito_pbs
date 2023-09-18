16479d67ec9a38d914056449645515bffbda9c59
Nils Rauch <rauch@puzzle.ch>
2023-09-18 15:00:32 +0200

    Adjust dropdown spec

--- spec/helpers/dropdown/event/camp_participant_status_spec.rb
+++ spec/helpers/dropdown/event/camp_participant_status_spec.rb
@@ -22,7 +22,8 @@ describe 'Dropdown::Event::CampParticipantStatus' do
   subject { dropdown.to_s }
 
   def link_to(label, path, options = {})
-    "<a href='#{path}'>#{label}</a>"
+    path ||= ''
+    super(label, path, options)
   end
 
   def can?(*args)
@@ -32,24 +33,14 @@ describe 'Dropdown::Event::CampParticipantStatus' do
   it 'renders dropdown without paper application' do
     is_expected.to have_content 'Status ändern'
     is_expected.to have_selector 'ul.dropdown-menu'
-    is_expected.to have_selector 'a' do |tag|
-      expect(tag).to have_selector 'strong' do |label|
-        expect(label).to have_content 'Zugewiesen'
-      end
-    end
-    is_expected.to have_selector 'a' do |tag|
-      expect(tag).to have_content 'Abgemeldet'
-    end
-    is_expected.to have_selector 'a' do |tag|
-      expect(tag).to have_content 'Nicht erschienen'
-    end
+    is_expected.to have_selector 'a', text: 'Angemeldet'
+    is_expected.to have_selector 'a', text: 'Abgemeldet'
+    is_expected.to have_selector 'a', text: 'Nicht erschienen'
     is_expected.not_to have_content 'Elektronisch angemeldet'
   end
 
   it 'renders dropdown with paper application' do
     event.update!(paper_application_required: true)
-    is_expected.to have_selector 'a' do |tag|
-      expect(tag).to have_content 'Elektronisch angemeldet'
-    end
+    is_expected.to have_content 'Elektronisch angemeldet'
   end
 end
