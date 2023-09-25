4abd8240059c041968b7a645f72372dc841bf911
Daniel Illi <daniel@illi.zone>
2023-09-25 16:25:42 +0200

    Add copyright comments

--- db/migrate/20230918160847_add_alumni_processing_timestamps_to_role.rb
+++ db/migrate/20230918160847_add_alumni_processing_timestamps_to_role.rb
@@ -1,4 +1,10 @@
+# encoding: utf-8
 # frozen_string_literal: true
+
+#  Copyright (c) 2023, Pfadibewegung Schweiz. This file is part of
+#  hitobito_pbs and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pbs.
 
 class AddAlumniProcessingTimestampsToRole < ActiveRecord::Migration[6.1]
   def up
