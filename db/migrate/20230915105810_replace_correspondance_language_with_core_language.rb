083b13d31d82c532572662c05dbbc9f1200cdcf3
Diego Steiner <diego.steiner@u041.ch>
2023-10-20 11:27:45 +0200

    migrate pbs language field intelligently

--- db/migrate/20230915105810_replace_correspondance_language_with_core_language.rb
+++ db/migrate/20230915105810_replace_correspondance_language_with_core_language.rb
@@ -1,7 +1,23 @@
 class ReplaceCorrespondanceLanguageWithCoreLanguage < ActiveRecord::Migration[6.1]
+  CANTONS_LANGUAGE = {
+    # de: [:ag, :ai, :ar, :be, :bl, :bs, :gl, :gr, :lu, :nw, :ow, :sg, :sh, :so, :sz, :tg, :ur, :zg, :zh],
+    fr: [:fr, :ge, :ju, :ne, :vd, :vs],
+    it: [:ti]
+  }.freeze
+
   def change
-    Person.where(correspondence_language: nil).update_all(correspondence_language: :de)
     Person.update_all("language = correspondence_language")
     remove_column :people, :correspondence_language, :string, limit: 5
+
+    Person.where(language: nil).find_each do |person|
+      person.update!(language: infer_person_language(person))
+    end
+  end  
+  
+  def infer_person_language(person)
+    return person.language if person.language.present?
+    
+    canton = person.find_kantonalverband&.kantonalverband_cantons&.first&.to_sym
+    CANTONS_LANGUAGE.find { |language, cantons| cantons.include?(canton) }&.first || :de
   end
 end
