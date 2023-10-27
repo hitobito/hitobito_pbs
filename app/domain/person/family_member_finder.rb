3e585028938a7270aef8c74ff7829a1e8fc8399a
Diego Steiner <939106+diegosteiner@users.noreply.github.com>
2023-10-27 14:59:28 +0200

    Update app/domain/person/family_member_finder.rb
    Co-authored-by: Carlo Beltrame <beltrame@puzzle.ch>

--- app/domain/person/family_member_finder.rb
+++ app/domain/person/family_member_finder.rb
@@ -1,7 +1,7 @@
-#  Copyright (c) 2018, Pfadibewegung Schweiz. This file is part of
-#  hitobito and licensed under the Affero General Public License version 3
+#  Copyright (c) 2023, Pfadibewegung Schweiz. This file is part of
+#  hitobito_pbs and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
-#  https://github.com/hitobito/hitobito.
+#  https://github.com/hitobito/hitobito_pbs.
 
 class Person::FamilyMemberFinder
   attr_reader :person
