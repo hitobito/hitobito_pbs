16e03cf7e801541a2b1a41cf4bc582eb02aa5e84
Nils Rauch <rauch@puzzle.ch>
2023-12-11 12:53:33 +0100

    Fix indentation

--- app/domain/person/family_member_finder.rb
+++ app/domain/person/family_member_finder.rb
@@ -10,25 +10,24 @@ class Person::FamilyMemberFinder
     @person = person
   end
 
-    def family_members_in_layer(group, kind: :sibling)
-      Role.joins(person: :family_members)
-          .where(group: group.groups_in_same_layer, 
-                 person: { family_members: { kind: kind, other: person } })
-    end
-    
-    def family_members_in_event(event, kind: :sibling)
-      Event::Participation.joins(person: :family_members)
-                   .where(person: { family_members: { kind: kind, other: person } },
-                          event: event)
-    end
+  def family_members_in_layer(group, kind: :sibling)
+    Role.joins(person: :family_members)
+      .where(group: group.groups_in_same_layer,
+             person: { family_members: { kind: kind, other: person } })
+  end
 
-    def family_members_in_context(context, kind: :sibling)
-      case context
-      when Event
-        family_members_in_event(context, kind: kind)
-      when Group
-        family_members_in_layer(context, kind: kind)
-      end
-    end
+  def family_members_in_event(event, kind: :sibling)
+    Event::Participation.joins(person: :family_members)
+      .where(person: { family_members: { kind: kind, other: person } },
+             event: event)
+  end
 
+  def family_members_in_context(context, kind: :sibling)
+    case context
+    when Event
+      family_members_in_event(context, kind: kind)
+    when Group
+      family_members_in_layer(context, kind: kind)
+    end
+  end
 end
