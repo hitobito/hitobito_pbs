52a9dd80c8ec05a4f41666681f62064b7dc01a6a
Diego Steiner <diego.steiner@u041.ch>
2023-12-15 15:54:08 +0100

    chore: update comment header

--- db/migrate/20230915105810_replace_correspondence_language_with_core_language.rb
+++ db/migrate/20230915105810_replace_correspondence_language_with_core_language.rb
@@ -1,3 +1,10 @@
+# frozen_string_literal: true
+
+# Copyright (c) 2023, Pfadibewegung Schweiz. This file is part of
+# hitobito_pbs and licensed under the Affero General Public License version 3
+# or later. See the COPYING file at the top-level directory or at
+# https://github.com/hitobito/hitobito_pbs.
+
 class ReplaceCorrespondenceLanguageWithCoreLanguage < ActiveRecord::Migration[6.1]
   CANTONS_LANGUAGE = {
     # de: [:ag, :ai, :ar, :be, :bl, :bs, :gl, :gr, :lu, :nw, :ow, :sg, :sh, :so, :sz, :tg, :ur, :zg, :zh],
