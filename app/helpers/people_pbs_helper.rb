69af2df8eaff97717bc9810bdb6eecaf9b93ef50
Carlo Beltrame <carlo@beltra.me>
2023-12-21 18:31:18 +0100

    Restore helper which is still in use

---
+++ app/helpers/people_pbs_helper.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2023, Pfadibewegung Schweiz. This file is part of
+#  hitobito_pbs and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_pbs.
+
+module PeoplePbsHelper
+
+  def format_person_salutation(person)
+    person.salutation_value
+  end
+
+end
