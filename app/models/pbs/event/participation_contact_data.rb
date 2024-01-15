d94fb31aac0f648b11f62cc89236967ed8bc84ce
Matthias Viehweger <viehweger@puzzle.ch>
2024-01-15 11:45:26 +0100

    Remove duplicated field
    After the conversion to the core-field, the manual addition is
    not needed anymore.

--- app/models/pbs/event/participation_contact_data.rb
+++ app/models/pbs/event/participation_contact_data.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-#  Copyright (c) 2012-2017, Pfadibewegung Schweiz. This file is part of
+#  Copyright (c) 2012-2024, Pfadibewegung Schweiz. This file is part of
 #  hitobito_pbs and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_pbs.
@@ -10,7 +10,7 @@ module Pbs::Event::ParticipationContactData
 
   included do
     Event::ParticipationContactData.contact_attrs << :title << :salutation <<
-     :language << :grade_of_school << :entry_date << :leaving_date
+     :grade_of_school << :entry_date << :leaving_date
 
     delegate(*Event::ParticipationContactData.contact_attrs, to: :person)
   end
