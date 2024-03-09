4a7cc8ff008adf87f9e3494d51dd94515742d1d6
simfeld <sml.imfeld@gmail.com>
2024-03-09 19:52:56 +0100

    Add validate_course_number column to event kinds

---
+++ db/migrate/20240309005706_add_validate_course_number_to_event_kinds.rb
@@ -0,0 +1,10 @@
+#  Copyright (c) 2024, Pfadibewegung Schweiz. This file is part of
+#  hitobito_cvp and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+class AddValidateCourseNumberToEventKinds < ActiveRecord::Migration[6.1]
+  def change
+    add_column :event_kinds, :validate_course_number, :boolean, default: false, null: false
+  end
+end
