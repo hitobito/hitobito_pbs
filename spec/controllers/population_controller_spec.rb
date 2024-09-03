291376e40bb3cc5b72d9878051586de34f5919a5
Andreas Maierhofer <maierhofer@puzzle.ch>
2024-09-03 16:50:10 +0200

    Adjust population_controller spec

--- spec/controllers/population_controller_spec.rb
+++ spec/controllers/population_controller_spec.rb
@@ -1,76 +1,72 @@
-# encoding: utf-8
-
 #  Copyright (c) 2012-2014, Pfadibewegung Schweiz. This file is part of
 #  hitobito_pbs and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_pbs.
 
-require 'spec_helper'
+require "spec_helper"
 
 describe PopulationController do
-
   let(:abteilung) { groups(:schekka) }
   let(:pegasus) { groups(:pegasus) }
-
 
   let!(:leader) { Fabricate(Group::Abteilung::Abteilungsleitung.name.to_sym, group: abteilung).person }
   let!(:guide) { Fabricate(Group::Abteilung::StufenleitungWoelfe.name.to_sym, group: abteilung).person }
   let!(:webmaster) { Fabricate(Group::Abteilung::Webmaster.name.to_sym, group: abteilung).person }
   let!(:deleted) do
     Fabricate(Group::Abteilung::AbteilungsleitungStv.name.to_sym,
-              group: abteilung,
-              created_at: 2.years.ago,
-              deleted_at: 1.year.ago)
+      group: abteilung,
+      created_at: 2.years.ago,
+      end_on: 1.year.ago)
   end
   let!(:group_leader) { Fabricate(Group::Pfadi::Mitleitung.name.to_sym, group: pegasus, person: guide).person }
   let!(:child) { Fabricate(Group::Pfadi::Pfadi.name.to_sym, group: pegasus).person }
 
   before { sign_in(leader) }
 
-  describe 'GET index' do
-    before { get :index, params: { id: abteilung.id } }
+  describe "GET index" do
+    before { get :index, params: {id: abteilung.id} }
 
-    describe 'groups' do
+    describe "groups" do
       subject { assigns(:groups) }
 
       it do
         is_expected.to eq([abteilung,
-                   groups(:sunnewirbu),        
-                   groups(:baereried),
-                   groups(:medusa),
-                   groups(:pegasus),
-                   groups(:poseidon),
-                   groups(:rovers),
-                   groups(:elternrat),
-                   groups(:fussballers)])
+          groups(:sunnewirbu),
+          groups(:baereried),
+          groups(:medusa),
+          groups(:pegasus),
+          groups(:poseidon),
+          groups(:rovers),
+          groups(:elternrat),
+          groups(:fussballers)])
       end
     end
 
-    describe 'people by group' do
+    describe "people by group" do
       subject { assigns(:people_by_group) }
-      
+
       it { expect(subject[abteilung].collect(&:to_s)).to contain_exactly(*[leader, people(:al_schekka), guide].collect(&:to_s)) }
       it { expect(subject[groups(:pegasus)].collect(&:to_s)).to contain_exactly(*[group_leader, child, people(:child)].collect(&:to_s)) }
       it { expect(subject[groups(:baereried)]).to be_nil } # no people in group - not displayed at all
     end
 
-    describe 'complete' do
+    describe "complete" do
       subject { assigns(:people_data_complete) }
 
       it { is_expected.to be_falsey }
     end
 
-    describe 'shows year data' do
+    describe "shows year data" do
       subject { assigns(:members_per_birthyear) }
 
       it { expect(subject.map(&:count).sum).to eq(assigns(:total).total) }
     end
   end
 
-  describe 'GET index does not include deleted groups' do
+  describe "GET index does not include deleted groups" do
     before do
       groups(:poseidon).destroy
-      get :index, params: { id: abteilung.id }
+      get :index, params: {id: abteilung.id}
     end
 
     subject { assigns(:groups) }
