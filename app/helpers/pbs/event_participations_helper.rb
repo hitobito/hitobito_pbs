ec1d1184275dc0df986e7728d8e62e49dac09a29
Niklas Jäggi <jaeggi@puzzle.ch>
2024-10-10 16:16:06 +0200

    Fix rubocop

--- app/helpers/pbs/event_participations_helper.rb
+++ app/helpers/pbs/event_participations_helper.rb
@@ -22,7 +22,7 @@ module Pbs
 
     def event_participation_table_options(t, event)
       if event.is_a?(Event::Camp)
-        t.col(Event::Participation.human_attribute_name(:state)) 		 { |p| t("activerecord.attributes.event/camp.participation_states.#{p.state}") }
+        t.col(Event::Participation.human_attribute_name(:state)) { |p| t("activerecord.attributes.event/camp.participation_states.#{p.state}") }
       end
     end
   end
