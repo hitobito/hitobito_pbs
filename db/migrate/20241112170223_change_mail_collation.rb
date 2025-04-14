0210d1b0e02c3f15a3a639fc2ca447dadb525df8
Andreas Maierhofer <a.maierhofer@gmail.com>
2025-04-14 13:53:23 +0200

    Remove case_insensitve collation (hitobito/hitobito#3153)

--- db/migrate/20241112170223_change_mail_collation.rb
+++ db/migrate/20241112170223_change_mail_collation.rb
@@ -8,18 +8,22 @@ class ChangeMailCollation < ActiveRecord::Migration[6.1]
   end
 
   def up
-    say_with_time "setting the case-insensitive collation" do
-      list_of_email_columns.each do |table, column|
-        execute "ALTER TABLE #{table} ALTER COLUMN #{column} SET DATA TYPE varchar COLLATE case_insensitive_emails;"
-      end
-    end
+    # noop since hitobito/hitobito#811b95eca
+    #
+    # say_with_time "setting the case-insensitive collation" do
+    #   list_of_email_columns.each do |table, column|
+    #     execute "ALTER TABLE #{table} ALTER COLUMN #{column} SET DATA TYPE varchar COLLATE case_insensitive_emails;"
+    #   end
+    # end
   end
 
   def down
-    say_with_time "setting the default collation" do
-      list_of_email_columns.each do |table, column|
-        execute "ALTER TABLE #{table} ALTER COLUMN #{column} SET DATA TYPE varchar;"
-      end
-    end
+    # noop since hitobito/hitobito#811b95eca
+    #
+    # say_with_time "setting the default collation" do
+    #   list_of_email_columns.each do |table, column|
+    #     execute "ALTER TABLE #{table} ALTER COLUMN #{column} SET DATA TYPE varchar;"
+    #   end
+    # end
   end
 end
