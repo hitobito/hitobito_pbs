1130899ce0d91b4aeee9692c544e6448192e2714
Jannik Pulfer <pulfer@puzzle.ch>
2026-01-08 15:57:25 +0100

    Revert to including event module

--- app/models/pbs/event.rb
+++ app/models/pbs/event.rb
@@ -8,12 +8,21 @@ module Pbs::Event
 
   J_S_KINDS_DATA_SHARING_ACCEPTANCE = %w[j_s_child j_s_youth j_s_mixed].freeze
 
-  prepended do
+  included do
     class_attribute :superior_attributes
     self.superior_attributes = []
 
     # dummy accessors for event_resource (only courses do have advisor)
     attr_accessor :advisor_id, :advisor
+
+    private
+
+    # Super cannot be called directly since the module redefines the original method
+    alias_method :original_attributes_for_duplicate, :attributes_for_duplicate
+
+    def attributes_for_duplicate
+      original_attributes_for_duplicate.excluding("lft", "rgt", "depth", "children_count", "parent_id")
+    end
   end
 
   def camp_submitted?
@@ -30,11 +39,5 @@ module Pbs::Event
   def j_s_data_sharing_acceptance_required?
     [Event::Camp.name, Event::Campy.name, Event::Course.name].include?(type) &&
       J_S_KINDS_DATA_SHARING_ACCEPTANCE.include?(j_s_kind)
-  end
-
-  private
-
-  def attributes_for_duplicate
-    super.excluding("lft", "rgt", "depth", "children_count", "parent_id")
   end
 end
