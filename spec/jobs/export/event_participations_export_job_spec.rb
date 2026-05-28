b89e3d1bccfb05c49442548f369c4bf735a2f7ae
Jannik Pulfer <pulfer@puzzle.ch>
2026-05-28 16:41:12 +0200

    Rename user job result to job observation

--- spec/jobs/export/event_participations_export_job_spec.rb
+++ spec/jobs/export/event_participations_export_job_spec.rb
@@ -17,7 +17,7 @@ describe Export::EventParticipationsExportJob do
       params.merge(filename: "event_participation_export"))
   }
 
-  let(:file) { subject.user_job_result }
+  let(:file) { subject.job_observation }
 
   before do
     SeedFu.quiet = true
